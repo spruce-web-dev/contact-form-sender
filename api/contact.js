fa60fc69166a483a65a5e83909914c29d43b0baf
JacopoFB <jacopofb03@gmail.com>
2026-05-18 11:45:30 -0600

    uploading work

---
+++ api/contact.js
@@ -0,0 +1,69 @@
+/* 
+    Small serverless function designed to work with resend and vercel servless functions.
+    They both have a generous free tier, so we should be able to send contact forms to our clients
+    without authentication hassle for probably only a few pennies per month.
+*/
+
+import { Resend } from "resend";
+import "dotenv/config"; // for local env variables
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+// Add client email + allowed fields here (NAMES MUST MATCH EXACTLY HOW YOU HAVE THEM IN THE FRONTEND)
+const CLIENTS = {
+    "jacopofb03@gmail.com": {
+        allowedFields: ["name", "phone", "email", "projectType", "message"]
+    }
+};
+
+export default async function (req, res) {
+    if (req.method !== "POST") { // only alllow post requests
+        return res.status(405).json({
+            error: "Method not allowed"
+        });
+    }
+
+    try {
+        const { destination, data } = req.body;
+
+        const client = CLIENTS[destination];
+
+        if (!client) {
+            return res.status(403).json({
+                error: "Destination not allowed"
+            });
+        }
+
+        const filtered = {};
+
+        for (const field of client.allowedFields) {
+            if (data[field] !== undefined) {
+                filtered[field] = data[field];
+            }
+        }
+
+        const html = Object.entries(filtered)
+            .map(([key, value]) => `<p><b>${key}</b>: ${value}</p>`)
+            .join("");
+
+        const result = await resend.emails.send({
+            from: "form-submissions@sprucedev.com",
+            //from: "onboarding@resend.dev", // resend testing domain
+            to: destination,
+            replyTo: filtered.email,
+            subject: "New Contact Form Submission",
+            html: `
+                <h2>New Submission</h2>
+                ${html}
+            `
+        });
+
+        return res.json({ success: true, result });
+    } catch (err) {
+        console.error(err);
+
+        return res.status(500).json({
+            error: "Send failed"
+        });
+    }
+}
